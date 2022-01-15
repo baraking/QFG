657e0aada1f61c8f9065bea7abaeae3450cbec4c
baraking <barakatz.bk@gmail.com>
2022-01-15 11:11:02 +0200

    added camera rotation

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,21 +7,29 @@ public class CameraController : MonoBehaviour
     public Transform target;
 
     public Vector3 offset;
-    public float pitch = 2f;
+    public float characterHeight = 2f;
 
-    private float currentZoom = 5f;
     public float zoomSpeed = 5f;
     public Vector2 zoomPositionRange = new Vector2(5, 10);
+
+    public float rotationSpeed = 100f;
+
+    private float currentZoom = 5f;
+    private float currentRotation = 0f;
 
     private void Update()
     {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         currentZoom = Mathf.Clamp(currentZoom, zoomPositionRange.x, zoomPositionRange.y);
+
+        currentRotation -= Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
     }
 
     private void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
-        transform.LookAt(target.position + Vector3.up * pitch);
+        transform.LookAt(target.position + Vector3.up * characterHeight);
+
+        transform.RotateAround(target.position, Vector3.up, currentRotation);
     }
 }
