3cf0d9c3444ce636b8679a1b0bada1fdf7cc18cd
baraking <barakatz.bk@gmail.com>
2022-01-15 10:52:10 +0200

    added basic movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void WalkToPoint(Vector3 point)
+    {
+        navMeshAgent.SetDestination(point);
+    }
+}
